eab4b0c6d4a996b52b7d983ccf7086f90d48e7b2
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-04-12 12:58:55 -0600

    Inicio de la estructura del proyecto.

---
+++ src/app/landing/pages/base-page/base-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'landing-base-page',
+  templateUrl: './base-page.component.html',
+  styleUrls: ['./base-page.component.scss']
+})
+export class BasePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
